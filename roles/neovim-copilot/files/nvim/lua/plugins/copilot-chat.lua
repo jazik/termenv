3dd7af0b5b4e313509e92c9f388992fcc9fc60fc
Jan Žižka <janzizka@yahoo.com>
2024-12-13 21:22:17 +0100

    Update copilot-chat to use main branch
    The canary branch is deprecated and main is recommended.
    Also add new build command recommended for Linux.
    Sync with plugin install instructions.

--- roles/neovim-copilot/files/nvim/lua/plugins/copilot-chat.lua
+++ roles/neovim-copilot/files/nvim/lua/plugins/copilot-chat.lua
@@ -1,11 +1,12 @@
 return {
   {
     "CopilotC-Nvim/CopilotChat.nvim",
-    branch = "canary",
+    branch = "main",
     dependencies = {
       { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
-      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log wrapper
     },
+    build = "make tiktoken",
     opts = {
       debug = true, -- Enable debugging
       -- See Configuration section for rest
