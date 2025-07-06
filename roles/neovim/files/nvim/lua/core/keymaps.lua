b06e77b140e150d06f66f4f6c40667495dfb0793
Jan Žižka <janzizka@yahoo.com>
2025-07-06 14:12:15 +0200

    Add 'fw' keybinding for nvim
    The 'fw' is implemented to grep word under cursor.

--- roles/neovim/files/nvim/lua/core/keymaps.lua
+++ roles/neovim/files/nvim/lua/core/keymaps.lua
@@ -53,6 +53,7 @@ keymap.set("n", "<leader>ef", ":NvimTreeFindFile<CR>") -- find file in file expl
 -- Telescope
 keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, {})
 keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep, {})
+keymap.set('n', '<leader>fw', function() require('telescope.builtin').grep_string({ search = vim.fn.expand("<cword>") }) end)
 keymap.set('n', '<leader>fb', require('telescope.builtin').buffers, {})
 keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags, {})
 keymap.set('n', '<leader>fs', require('telescope.builtin').current_buffer_fuzzy_find, {})
